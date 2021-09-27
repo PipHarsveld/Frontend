d6f8feb8921ce75ff791b787e876bbec4e02b232
Pip <54938035+PipHarsveld@users.noreply.github.com>
2021-09-27 11:03:46 +0200

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,7 @@
 // JavaScript Document
 var menuOpenenIcon = document.querySelector("figure");
 var uitgeklaptMenu = document.querySelector("main nav")
-var MenuSluitenIcon = document.querySelector("main nav ul li img")
+var MenuSluitenIcon = document.querySelector("main nav img:nth-of-type(2)")
 
 //Menu openen
 menuOpenenIcon.addEventListener("click", toggleMenu);
