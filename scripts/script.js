2b42f185797e7548f4b0029ab8d26a6904f502f3
Pip <54938035+PipHarsveld@users.noreply.github.com>
2021-09-30 22:57:02 +0200

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,21 @@
+// // JavaScript Document
+// var menuOpenenIcon = document.querySelector("header nav ul li button");
+// var uitgeklaptMenu = document.querySelector("main section:nth-of-type(7) nav");
+// var menuSluitenIcon = document.querySelector("main section:nth-of-type(7) nav img:nth-of-type(2)");
+
+// //Menu openen
+// menuOpenenIcon.addEventListener("click", toggleMenu);
+// //Menu sluiten
+// menuSluitenIcon.addEventListener("click", toggleMenu);
+
+// function toggleMenu(){
+//     uitgeklaptMenu.classList.toggle("uitgeklaptMenu");
+// }
+
 // JavaScript Document
-var menuOpenenIcon = document.querySelector("header nav ul li button");
-var uitgeklaptMenu = document.querySelector("main section:nth-of-type(7) nav");
-var menuSluitenIcon = document.querySelector("main section:nth-of-type(7) nav img:nth-of-type(2)");
+var menuOpenenIcon = document.querySelector("header > button");
+var uitgeklaptMenu = document.querySelector("header nav");
+var menuSluitenIcon = document.querySelector("header nav button");
 
 //Menu openen
 menuOpenenIcon.addEventListener("click", toggleMenu);
@@ -10,5 +24,4 @@ menuSluitenIcon.addEventListener("click", toggleMenu);
 
 function toggleMenu(){
     uitgeklaptMenu.classList.toggle("uitgeklaptMenu");
-    console.log("Testttt");
 }
