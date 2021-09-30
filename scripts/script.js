f513279798b67c8c333eb01752c34773cfd0e764
Pip <54938035+PipHarsveld@users.noreply.github.com>
2021-09-30 15:43:46 +0200

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,15 +1,14 @@
 // JavaScript Document
-var menuOpenenIcon = document.querySelector("figure");
-var uitgeklaptMenu = document.querySelector("main nav")
-var MenuSluitenIcon = document.querySelector("main nav img:nth-of-type(2)")
+var menuOpenenIcon = document.querySelector("header nav ul li button");
+var uitgeklaptMenu = document.querySelector("main section:nth-of-type(7) nav");
+var menuSluitenIcon = document.querySelector("main section:nth-of-type(7) nav img:nth-of-type(2)");
 
 //Menu openen
 menuOpenenIcon.addEventListener("click", toggleMenu);
 //Menu sluiten
-MenuSluitenIcon.addEventListener("click", toggleMenu);
+menuSluitenIcon.addEventListener("click", toggleMenu);
 
 function toggleMenu(){
     uitgeklaptMenu.classList.toggle("uitgeklaptMenu");
+    console.log("Testttt");
 }
-
-
